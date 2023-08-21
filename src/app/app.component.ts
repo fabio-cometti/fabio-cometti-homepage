8aa0de465b89a7ee5a9d5fc75ea34891a844b7c9
Fabio Cometti <fabio.cometti@gmail.com>
2023-08-21 14:42:31 +0200

    test performance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { ScrollManagerDirective } from './directives/scroll-manager.directive';
 import { ScrollSectionDirective } from './directives/scroll-section.directive';
 import { ScrollAnchorDirective } from './directives/scroll-anchor.directive';
+import { timer } from 'rxjs';
 
 
 @Component({
@@ -13,56 +14,56 @@ import { ScrollAnchorDirective } from './directives/scroll-anchor.directive';
   standalone: true,
   imports: [CommonModule, DynamicSection, FontAwesomeModule, ScrollManagerDirective, ScrollSectionDirective, ScrollAnchorDirective],
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],  
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements AfterViewInit {
-  
+
   showMenu: boolean = false;
   faBars = faBars;
-  
+
   @ViewChildren(DynamicSection) sectionHosts!:  QueryList<DynamicSection>;
-  
+
   ngAfterViewInit(): void {
-    this.loadComponents();
+    timer(3000).subscribe( _ => this.loadComponents());
   }
 
   loadComponents() {
-    
-    import('./sections/about-me/about-me.component').then(({ AboutMeComponent}) => {        
-      this.initComponent(0, AboutMeComponent);      
-    }); 
-    
-    import('./sections/skills/skills.component').then(({ SkillsComponent}) => {        
-      this.initComponent(1, SkillsComponent);      
+
+    import('./sections/about-me/about-me.component').then(({ AboutMeComponent}) => {
+      this.initComponent(0, AboutMeComponent);
     });
-    
-    import('./sections/work-experiences/work-experiences.component').then(({ WorkExperiencesComponent}) => {        
-      this.initComponent(2, WorkExperiencesComponent);      
-    }); 
 
-    import('./sections/projects/projects.component').then(({ ProjectsComponent }) => {        
-      this.initComponent(3, ProjectsComponent );      
-    }); 
-
-    import('./sections/open-source/open-source.component').then(({ OpenSourceComponent }) => {        
-      this.initComponent(4, OpenSourceComponent);      
-    }); 
-
-    import('./sections/education/education.component').then(({ EducationComponent }) => {        
-      this.initComponent(5, EducationComponent);      
+    import('./sections/skills/skills.component').then(({ SkillsComponent}) => {
+      this.initComponent(1, SkillsComponent);
     });
-    
-    import('./sections/certifications/certifications.component').then(({ CertificationsComponent }) => {        
-      this.initComponent(6, CertificationsComponent);            
-    }); 
 
-    import('./sections/interests/interests.component').then(({ InterestsComponent }) => {        
-      this.initComponent(7, InterestsComponent);      
-    }); 
+    import('./sections/work-experiences/work-experiences.component').then(({ WorkExperiencesComponent}) => {
+      this.initComponent(2, WorkExperiencesComponent);
+    });
 
-    import('./sections/contacts/contacts.component').then(({ ContactsComponent }) => {        
-      this.initComponent(8, ContactsComponent);      
-    }); 
+    import('./sections/projects/projects.component').then(({ ProjectsComponent }) => {
+      this.initComponent(3, ProjectsComponent );
+    });
+
+    import('./sections/open-source/open-source.component').then(({ OpenSourceComponent }) => {
+      this.initComponent(4, OpenSourceComponent);
+    });
+
+    import('./sections/education/education.component').then(({ EducationComponent }) => {
+      this.initComponent(5, EducationComponent);
+    });
+
+    import('./sections/certifications/certifications.component').then(({ CertificationsComponent }) => {
+      this.initComponent(6, CertificationsComponent);
+    });
+
+    import('./sections/interests/interests.component').then(({ InterestsComponent }) => {
+      this.initComponent(7, InterestsComponent);
+    });
+
+    import('./sections/contacts/contacts.component').then(({ ContactsComponent }) => {
+      this.initComponent(8, ContactsComponent);
+    });
   }
 
   toggleMenu(): void {
@@ -74,8 +75,8 @@ export class AppComponent implements AfterViewInit {
   }
 
   private initComponent(index: number, type: Type<unknown>) : void {
-    const viewContainerRef = this.sectionHosts.get(index)!.viewContainerRef;    
-      viewContainerRef.clear();  
+    const viewContainerRef = this.sectionHosts.get(index)!.viewContainerRef;
+      viewContainerRef.clear();
       const cRef = viewContainerRef.createComponent(type);
   }
 }
