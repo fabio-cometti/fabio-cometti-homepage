c299e683e45b264792a8e5b5a82f9c0652ceae94
Fabio Cometti <fabio.cometti@gmail.com>
2023-08-31 15:05:58 +0200

    add project section

--- src/app/sections/projects/projects.component.ts
+++ src/app/sections/projects/projects.component.ts
@@ -2,11 +2,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ObserveVisibilityDirective } from 'src/app/directives/observe-visibility.directive';
 import { Title } from '@angular/platform-browser';
+import { AboutBlockComponent } from 'src/app/components/about-block/about-block.component';
+import { ScrollSectionDirective } from 'src/app/directives/scroll-section.directive';
 
 @Component({
   selector: 'fc-projects',
   standalone: true,
-  imports: [CommonModule, ObserveVisibilityDirective],
+  imports: [CommonModule, ObserveVisibilityDirective, AboutBlockComponent, ScrollSectionDirective],
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
