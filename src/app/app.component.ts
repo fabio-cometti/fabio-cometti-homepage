2e7816b962d96ebb3864718a1d6b11530416122a
Fabio Cometti <fabio.cometti@gmail.com>
2023-10-02 19:55:00 +0200

    test performance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -67,7 +67,7 @@ export class AppComponent implements AfterViewInit, OnInit {
   }
 
   ngAfterViewInit(): void {
-    timer(3000).subscribe( _ => this.loadComponents());
+    timer(100).subscribe( _ => this.loadComponents());
   }
 
   loadComponents() {
