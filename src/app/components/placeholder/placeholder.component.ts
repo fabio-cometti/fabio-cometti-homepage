a65dc6e7ab0535fcee58b6d34ae282bff95118f8
Fabio Cometti <fabio.cometti@gmail.com>
2023-11-17 17:09:26 +0100

    fix placeholder component

--- src/app/components/placeholder/placeholder.component.ts
+++ src/app/components/placeholder/placeholder.component.ts
@@ -8,13 +8,28 @@ import { faImage } from '@fortawesome/free-solid-svg-icons';
   standalone: true,
   imports: [CommonModule, FontAwesomeModule],
   template: `
-    <div><fa-icon [icon]="faImage"></fa-icon></div>
+    <div class="outer-placeholder"><div class="inner-placeholder blink"><fa-icon [icon]="faImage"></fa-icon></div></div>
   `,
   styles: `
-    div {
+    .outer-placeholder {
       width: 100%;
-      height: 4em;
+      height: 20em;
       text-align: center;
+
+      .inner-placeholder{
+        display: inline-block;
+        width: 20em;
+        color: var(--accent);
+      }
+
+      .blink {
+        animation: blink 1s linear infinite;
+      }
+      @keyframes blink{
+        0%{opacity: 0;}
+        50%{opacity: .5;}
+        100%{opacity: 1;}
+      }
     }
   `
 })
