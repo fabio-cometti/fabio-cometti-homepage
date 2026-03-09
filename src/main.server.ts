7d70dbd11dac2307300fc41cc42c9e5c80d2f6cc
Fabio Cometti <fabio.cometti@one4.it>
2026-03-09 08:12:40 +0100

    fix server config

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { config } from './app/app.config.server';
 import { RouterWrapperComponent } from './app/components/router-wrapper/router-wrapper.component';
+import { config as faConfig } from '@fortawesome/fontawesome-svg-core';
+
+faConfig.autoAddCss = false;
 
 const bootstrap = (context: BootstrapContext) => bootstrapApplication(RouterWrapperComponent, config, context);
 
